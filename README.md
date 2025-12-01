8c7eb58ae0a0d3ff0fe802d4ee9ca468e41adc70
Boglarka Weltz <weltz.boglarka@gmail.com>
2025-12-01 10:15:02 +0100

    Add readme and template file

---
+++ README.md
@@ -0,0 +1,1 @@
+:heart: :star: :ribbon: :santa: :christmas_tree: Advent of Code 2025 :christmas_tree: :santa: :ribbon: :star: :heart:
---
+++ day.php
@@ -0,0 +1,26 @@
+<?php
+
+$input = "";
+
+$input = "";
+
+function day1($input) {
+  $lines = explode("\n", trim($input));
+  $res = 0;
+  for ($i = 0; $i < sizeof($lines); $i++) {
+  }
+
+  return $res;
+}
+
+function day2($input) {
+  $lines = explode("\n", trim($input));
+  $res = 0;
+  for ($i = 0; $i < sizeof($lines); $i++) {
+  }
+
+  return $res;
+}
+
+echo day1($input);
+//echo day2($input);
